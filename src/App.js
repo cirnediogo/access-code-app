4e634c3320b18a359ce742032cbe0d781c3aecb0
Diogo Cirne <cirnediogo@gmail.com>
2019-08-19 20:52:21 -0300

    App.js changed from component to container and Routes added.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Register from './containers/Register/Register';
+import Auth from './components/Auth/Auth';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/register" component={Register} />
+            <Route path="/auth" component={Auth} />
+            <Redirect from="/" to="/auth" />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const auth = (props) => {
+
+    return (
+        <div className='Auth'>
+            <h1>Auth component</h1>
+        </div>
+    )
+
+}
+
+export default auth;
