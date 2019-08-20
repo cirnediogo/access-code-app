6de36836b40a00f2a7c332408b9e2d553613e5d4
Diogo Cirne <cirnediogo@gmail.com>
2019-08-20 19:23:33 -0300

    Styled the Auth page.

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
+import Input from 'muicss/lib/react/input';
+import Button from 'muicss/lib/react/button';
+
+import './Auth.css';
 
 const auth = (props) => {
 
     return (
         <div className='Auth'>
-            <h1>Auth component</h1>
+            <div >
+                <h1>Bem-vindo</h1>
+                <p>Utilize o seu código de acesso para entrar.</p>
+                <form>
+                    <Input label="Código de acesso" floatingLabel={true} />
+                    <Button variant="raised" color="primary">Entrar</Button>
+                </form>
+                <p className='soft'>
+                    O código de acesso é aquele gerado após feito o cadastro e enviado para o e-mail informardo.
+                </p>
+                <p>
+                    Ainda não possui um código de acesso?
+                </p>
+                <Link to="/register">
+                    <Button variant="flat" color="primary">Cadastrar</Button>
+                </Link>
+            </div>
         </div>
     )
 
